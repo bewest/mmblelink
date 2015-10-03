628dd1992eb3e77e361bbbfedb8c4c23e711ba26
Ben West <bewest@gmail.com>
2015-10-03 16:03:08 -0700

    try encoding RF messages using 4b6b encoding
    Hat tip to @ps2 and others. :-)

--- mmblelink/handlers/pump.py
+++ mmblelink/handlers/pump.py
@@ -37,6 +37,63 @@ class PumpEmulate (object):
       if packet.serial == self.serial and packet.type == 0xA7:
         print "RECEIVED REQUEST", packet
 
+class FourBySix (object):
+  SYMBOLS = {
+    "010101" : "0",
+    "110001" : "1",
+    "110010" : "2",
+    "100011" : "3",
+    "110100" : "4",
+    "100101" : "5",
+    "100110" : "6",
+    "010110" : "7",
+    "011010" : "8",
+    "011001" : "9",
+    "101010" : "a",
+    "001011" : "b",
+    "101100" : "c",
+    "001101" : "d",
+    "001110" : "e",
+    "011100" : "f"
+  }
+
+  CODES = [
+    0b010101,
+    0b110001,
+    0b110010,
+    0b100011,
+    0b110100,
+    0b100101,
+    0b100110,
+    0b010110,
+    0b011010,
+    0b011001,
+    0b101010,
+    0b001011,
+    0b101100,
+    0b001101,
+    0b001110,
+    0b011100
+  ]
+  @classmethod
+  def encode (klass, buf):
+    codes = [ ]
+    for b in list(buf):
+      codes.append(klass.CODES[( b >> 4 )])
+      codes.append(klass.CODES[( b & 0xf )])
+    bits = [ ]
+    for code in codes:
+      bits.append("{:06b}".format(code))
+    bits = ''.join(bits) + "000000000000"
+    remaining = bits[:]
+    out = [ ]
+    while len(remaining) > 7:
+      byte_bits, remaining = remaining[0:8], remaining[8:]
+      out.append(int(byte_bits, 2))
+    return bytearray(out)
+
+  def decode (klass, buf):
+    pass
 class States (object):
   responders = dict( )
   queue = [ ]
@@ -74,15 +131,19 @@ class States (object):
       buf = resp.respond(payload)
       print "SENDING", str(buf).encode('hex')
       print lib.hexdump(buf)
-      link.write(buf)
-      link.triggerTX( )
-      link.triggerTX( )
+      encoded =  FourBySix.encode(buf)
+      print "SENDING ENCODED", str(encoded).encode('hex')
+      print lib.hexdump(encoded)
+      # link.write(buf)
+      link.write(encoded)
       link.triggerTX( )
       
   def onReadPumpModel (self, packet):
-    body = bytearray(self.model) + bytearray([0x00]) 
-    # payload = bytearray([len(body) + 5, len(self.model)]) + body
+    # body = bytearray(self.model)
+    body = bytearray(self.model) + bytearray([0x00, 0x00, 0x00 ]) 
     payload = bytearray([len(body) + 3, len(self.model)]) + body
+    # payload = bytearray([0x09, len(self.model)]) + body
+    # payload = bytearray([len(self.model)]) + body
     missing = [ ]
     # missing = bytearray([0x00]) * (65 - len(payload))
     return payload + bytearray(missing)
@@ -132,6 +193,9 @@ if __name__ == '__main__':
   from mmblelink.link import Link
   parser = setup_argparser( )
   args = parser.parse_args( )
+  print "radio"
+  print lib.hexdump(FourBySix.encode(bytearray(str('a79090908d090336363680').decode('hex'))))
+  
   if args.verbosity > 0:
     print args
   if args.mac and args.serial:
