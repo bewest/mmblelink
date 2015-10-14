4a2f58005888b48539f49db0922f9129f5c2f33c
Ben West <bewest@gmail.com>
2015-10-14 00:44:36 -0700

    be more neighborly, include setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='mmblelink',
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=["python-dateutil", "decocare", "gattlib"],
+)
