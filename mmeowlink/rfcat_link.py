e08f69251dff30c17508d30a1a6a4ef0136ddc08
Ben West <bewest@gmail.com>
2015-11-15 13:03:14 -0800

    make medtronic logic local to this module

--- mmeowlink/rfcat_link.py
+++ mmeowlink/rfcat_link.py
@@ -12,6 +12,63 @@ from rflib import *
 class NotImplementedException (Exception):
   pass
 
+
+def setup_medtronic_mmcommander (self, locale='US'):
+    self.getRadioConfig()
+    rc = self.radiocfg
+    rc.sync1 = 0xff
+    rc.sync0 = 0x00
+    rc.pktlen = 0xff
+    rc.pktctrl1 = 0x00
+    rc.pktctrl0 = 0x00
+    rc.addr = 0x00
+    rc.channr = 0x00
+    rc.fsctrl1 = 0x06
+    rc.fsctrl0 = 0x00
+    # EU:
+    rc.freq2      = 0x24
+    rc.freq1      = 0x2e
+    rc.freq0      = 0x38
+
+    # US
+    if locale == 'US':
+      rc.freq2      = 0x26 # OK
+      rc.freq1      = 0x30 # OK
+      rc.freq0      = 0x00 # OK
+
+    rc.mdmcfg4    = 0xb9 # OK
+    rc.mdmcfg3    = 0x66 # OK
+    rc.mdmcfg2    = 0x33 # OK
+    rc.mdmcfg1    = 0x61 # OK
+    rc.mdmcfg0    = 0xe6 # OK
+    rc.deviatn    = 0x15 # OK
+    rc.mcsm2      = 0x07
+    rc.mcsm1      = 0x30
+    rc.mcsm0      = 0x18
+    rc.foccfg     = 0x17
+    rc.bscfg      = 0x6c
+    rc.agcctrl2   = 0x03
+    rc.agcctrl1   = 0x40
+    rc.agcctrl0   = 0x91
+    rc.frend1     = 0x56
+    rc.frend0     = 0x11 # OK                      Which pa_table item matches
+    rc.fscal3     = 0xe9
+    rc.fscal2     = 0x2a
+    rc.fscal1     = 0x00
+    rc.fscal0     = 0x1f
+    rc.test2      = 0x88
+    rc.test1      = 0x31
+    rc.test0      = 0x09
+    rc.pa_table7  = 0x00
+    rc.pa_table6  = 0x00
+    rc.pa_table5  = 0x00
+    rc.pa_table4  = 0x00
+    rc.pa_table3  = 0x00
+    rc.pa_table2  = 0x00
+    rc.pa_table1  = 0x50 # 52 in USA?
+    rc.pa_table0  = 0x00
+    self.setRadioConfig()
+
 class Link( object ):
   __timeout__ = .500
   port = None
@@ -30,7 +87,8 @@ class Link( object ):
     if not self.rfcat:
       print("Loading rfcat")
       self.rfcat = RfCat()
-      self.rfcat.setup_medtronic_mmcommander_eu()
+      self.rfcat
+      setup_medtronic_mmcommander(self.rfcat)
 
     log.info( '{agent} started RfCat library'
       .format(agent=self.__class__.__name__ ))
